7a4f3eaa349210ff4f102e9fbb1136d27327c135
wachin <wachin.id@gmail.com>
2021-04-12 20:51:45 -0500

    update

--- record_my_desktop.sh
+++ record_my_desktop.sh
@@ -6,4 +6,7 @@
 #rm -f ~/Video/output.mkv
 Xaxis=$(xrandr -q | grep '*' | uniq | awk '{print $1}' |  cut -d 'x' -f1)
 Yaxis=$(xrandr -q | grep '*' | uniq | awk '{print $1}' |  cut -d 'x' -f2)
-avconv -f x11grab -s $(($Xaxis))x$(($Yaxis)) -r 25 -i :0.0 ~/Videos/output.mkv
+ffmpeg -video_size $(($Xaxis))x$(($Yaxis)) -r 60 -f x11grab -i :0.0 \
+       -f alsa -ac 2 -i default \
+       -c:v libx264 -qp 0 -preset ultrafast \
+       "$HOME/Rec-$(date '+%Y-%m-%d_%H.%M.%S').mp4"
